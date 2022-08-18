f4360efd9a27fefc11196f14082c5eead5ac8b89
Ercan ESE <ercan.ese@bilgeadam.com>
2022-08-18 23:10:34 +0300

    add elastic

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
 using mercedes.Models;
 using mercedes.Models.Entities;
 using Microsoft.EntityFrameworkCore;
-
+using Elastic.Apm;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Elastic.Apm.NetCoreAll;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddOpenTelemetryTracing(configuration =>
-{
-    configuration
-        .AddAspNetCoreInstrumentation()
-        .SetResourceBuilder(ResourceBuilder.CreateDefault()
-            .AddService("MyWebApp")
-            .AddTelemetrySdk())
-        .AddOtlpExporter(options =>
-        {
-            options.Endpoint = new Uri("http://172.17.0.4:4317"); // SigNoz Endpoint
-        });
-});
+// builder.Services.AddOpenTelemetryTracing(configuration =>
+// {
+//     configuration
+//         .AddAspNetCoreInstrumentation()
+//         .SetResourceBuilder(ResourceBuilder.CreateDefault()
+//             .AddService("MyWebApp")
+//             .AddTelemetrySdk())
+//         .AddOtlpExporter(options =>
+//         {
+//             options.Endpoint = new Uri("http://172.17.0.4:4317"); // SigNoz Endpoint
+//         });
+// });
 
 
 IConfiguration configuration = new ConfigurationBuilder()
@@ -37,6 +38,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseAllElasticApm(configuration);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
