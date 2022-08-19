3701899600addd98d659927c64d370262aca5961
Ercan ESE <ercan.ese@bilgeadam.com>
2022-08-19 16:26:08 +0300

    add elastic 4

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseAllElasticApm(configuration);
+app.UseAllElasticApm(builder.Configuration);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
